c2f988c25b636c88382aee27155f05a396274023
Benjamin Kim <bkim@BSD-Recovery-Macbook-Air.local>
2017-04-23 15:55:10 -0400

    fixed

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.listen(port, function(){
 	console.log('listening on port ' + port);
 });
 
-app.post('/hello', function(req, res, next) {
+app.post('/dave', function(req, res, next) {
 	var userName = req.body.user_name;
 	var botPayload = {
-		text: "Just do it " + userName + ". - Dave"
+		text: "Let me tell you something you already know. The world ain't all sunshine and rainbows. It is a very mean and nasty place and it will beat you to your knees and keep you there permanently if you let it. You, me, or nobody is gonna hit as hard as life. But it ain't how hard you hit; it's about how hard you can get hit, and keep moving forward. How much you can take, and keep moving forward. That's how winning is done. Now, if you know what you're worth, then go out and get what you're worth. But you gotta be willing to take the hit, and not pointing fingers saying you ain't where you are because of him, or her, or anybody. Cowards do that and that ain't you. You're better than that " + userName + ". - Dave"
 	};
 
 	if (userName !== 'slackbot') {
