59b44433b396da1adc950ff8be7e8fba65785b8b
91juhwang <91juhwang@gmail.com>
2017-06-10 16:41:38 -0400

    Add more keyword response

--- app.js
+++ app.js
@@ -12,11 +12,19 @@ app.listen(port, function(){
 	console.log('listening on port ' + port);
 });
 
-app.post('/dave', function(req, res, next) {
+app.post('/keywords', function(req, res, next) {
 	var userName = req.body.user_name;
-	var botPayload = {
-		text: "Hey " + userName + " keep being awesome. - Dave"
-	};
+	var keyword = req.body.trigger_word;
+
+	if (keyword == 'dave') {
+		var botPayload = {
+			text: "Hey " + userName + " keep being awesome. - Dave"
+		}
+	} else if (keyword == 'cohort') {
+		var botPayload = {
+			text: "The Best Cohort Ever! Hope you guys the best!!"
+		}
+	}
 
 	if (userName !== 'slackbot') {
 		return res.status(200).json(botPayload);
